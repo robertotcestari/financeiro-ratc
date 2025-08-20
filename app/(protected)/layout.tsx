137310f0009c3a2a82aa7ac55f1044d6ebe726cb
Roberto Cestari <robertotcestari@gmail.com>
2025-08-20 09:30:41 -0300

    make layout dinamico

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -1,13 +1,11 @@
-import AuthGuard from "@/app/components/AuthGuard";
+import AuthGuard from '@/app/components/AuthGuard';
+
+export const dynamic = 'force-dynamic';
 
 export default function ProtectedLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <AuthGuard>
-      {children}
-    </AuthGuard>
-  );
+  return <AuthGuard>{children}</AuthGuard>;
 }
